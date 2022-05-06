858b203c6025477cbddbfb487d1bbcff4579e487
QuannBluee <quannguyenthanh558@gmail.com>
2022-05-06 19:55:07 +0700

    remove redundent folder

--- dist/people/people.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const peopleSchema = new mongoose_1.Schema({
-    name: String,
-    email: String,
-    phone: String,
-}, { timestamps: true });
-module.exports = (0, mongoose_1.model)("people", peopleSchema);
